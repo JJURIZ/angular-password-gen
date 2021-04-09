6980657d9df186866253e1f132bb44433119cd38
Jeremy Uriz <jjuriz@gmail.com>
2021-04-09 07:47:27 -0400

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pw';
+  //properties
+  password:string = ''
+  length:number= 0
+  includeLetters:boolean = false
+  includeNumbers:boolean = false
+  includeSymbols:boolean = false
+
+  //methods
+  onButtonClick() {
+    this.password = 'YOUR PASSWORD'
+    console.log(`
+      About to generate a password with the following:
+      Includes letters: ${this.includeLetters}
+      Includes numbers: ${this.includeNumbers}
+      Includes symbols: ${this.includeSymbols}
+      The length is: ${this.length}
+    `)
 }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols
+  }
+
+  onChangeLength(value:string) {
+    const parsedValue = parseInt(value);
+
+    if(!isNaN(parsedValue)) {
+      this.length = parsedValue
+    }
+    
+  }
+
+}
+
+
+// event binding, property binding, interpolation
